fb1746bf542c29d2cdaa9f781c1f0a945eb26552
Alisa Savorskaya <savorskaya.alisa@gmail.com>
2025-11-13 14:42:48 +0300

    fix: prettier fixes

--- src/libs/clipboard.ts
+++ src/libs/clipboard.ts
@@ -1,27 +1,24 @@
 export function copyToClipboard(str: string, cb: (status: boolean) => void) {
-  if (
-    navigator.clipboard &&
-    typeof navigator.clipboard.writeText === 'function'
-  ) {
+  if (navigator.clipboard && typeof navigator.clipboard.writeText === 'function') {
     navigator.clipboard
       .writeText(str)
       .then(() => cb(true))
-      .catch(() => cb(false))
+      .catch(() => cb(false));
   } else {
     try {
-      const textarea = document.createElement('textarea')
-      textarea.value = str
-      textarea.style.position = 'fixed' // предотвратить скролл
-      textarea.style.opacity = '0' // не видно на странице
-      document.body.appendChild(textarea)
-      textarea.focus()
-      textarea.select()
+      const textarea = document.createElement('textarea');
+      textarea.value = str;
+      textarea.style.position = 'fixed'; // предотвратить скролл
+      textarea.style.opacity = '0'; // не видно на странице
+      document.body.appendChild(textarea);
+      textarea.focus();
+      textarea.select();
 
-      const successful = document.execCommand('copy')
-      document.body.removeChild(textarea)
-      cb(successful)
+      const successful = document.execCommand('copy');
+      document.body.removeChild(textarea);
+      cb(successful);
     } catch {
-      cb(false)
+      cb(false);
     }
   }
 }
