956fdf2494d870c5f8d81c121f5276a264cb1e8e
Alisa Savorskaya <savorskaya.alisa@gmail.com>
2025-11-13 15:02:35 +0300

    fix: FileList, deleted unused packages

--- src/sidepanel/components/ChatContainer.tsx
+++ src/sidepanel/components/ChatContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { ChatMessage as ChatMessageType } from '../../types';
+import { ChatMessage as ChatMessageType } from '@/types';
 import ChatMessage from './ChatMessage';
 import styles from './ChatContainer.module.css';
 
